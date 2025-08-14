6cfa32fb9f2e9c1c68494d678993b1642fd01502
LaysArceles <LaysArceles12@gmail.com>
2025-08-14 11:35:08 -0300

    (aula3)react

---
+++ React/aula react/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Bosta</h1>
+      <h2>Merda</h2>
+      <h3>Odio</h3>
+    </>
+  )
+}
+
+export default App
