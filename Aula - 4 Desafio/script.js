e878910663547fba77ff97d9213502e802b7931d
LaysArceles <LaysArceles12@gmail.com>
2025-08-20 11:46:20 -0300

    Aula 4 - (Desafio da aula)

---
+++ Aula - 4 Desafio/script.js
@@ -0,0 +1,8 @@
+
+
+const TrocarCor = () =>{
+    let ler = document.getElementById('Select');
+    let lerr = queryselect(ler);
+    lerr.classList.add(ler);
+
+}
--- Aula - 4 JavaScript/script.js
+++ Aula - 4 JavaScript/script.js
@@ -1,8 +1,46 @@
-let varlet = 100 // escopo de função local
-var variavel = 10 // escopo global 
-const varconst = 1// igual a defaine
-let Varundefined = undefined
+// let varlet = 100 // escopo de função local
+// var variavel = 10 // escopo global 
+// const varconst = 1// igual a defaine
+// let Varundefined = undefined
 
-varconst = 10
+// const nome = "JuJu^-^";
+// const Idade = 17;
+// let Divas = ["Letição", "MaluS2", "Jheni>_<", "Las<3"];
+// const obj =
+// {
+//     nome: "Lays",
+//     Idade: 17,
+//     Trabalho: "Bosch"
+// } // objeto 
 
-// array
+// function soma(x, y) { 
+//     return x + y; 
+// } // função normal
+
+// const somar = function (a, b) {
+//      return a - b; 
+// } // função anonima 
+
+// const sub = (a, b) => (a * b);// função flacha
+
+// console.log(nome + " Tem " + Idade + " Anos ");
+// console.log(Divas[0]);
+// Divas.forEach((element) => console.log(element));
+// console.log(obj);
+// console.log(sub(4, 5));
+// console.log(somar(6, 4));
+
+// let title = document.getElementById('EL1').innerHTML;// só o elemento e sem e o tudo
+// console.log(title);
+
+// const ReceberTexto = () =>{
+//     let inputtitle = document.getElementById('EL2').value;
+//     console.log(inputtitle);
+// }
+
+const TrocarCor = () =>{
+    let Redon = document.getElementById('Redondo');
+    Redon.classList.remove('ok')
+    Redon.classList.add('bad');
+
+}
