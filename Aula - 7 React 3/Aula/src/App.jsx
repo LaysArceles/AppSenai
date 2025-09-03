0a98302917ac7b7cf48ce436a55da8179d2544f8
unknown <LaysArceles12@gmail.com>
2025-09-03 09:12:42 -0300

    Corrigindo exercicio 2 do UseState

--- Aula - 7 React 3/Aula/src/App.jsx
+++ Aula - 7 React 3/Aula/src/App.jsx
@@ -5,10 +5,10 @@ import './App.css'
 
 function App() {
   const [visibilty, setvisibility] = useState(true)
+  const [color,setColor] = useState("rebeccapurple")
 
   return (
     <>
-
       <div className='cartao'>
       { visibilty &&
         <div>
@@ -20,6 +20,19 @@ function App() {
       }
         <button onClick={() => {setvisibility(visibilty == true ? false: true)}}>visibilty</button>  
     </div>
+
+    <div className= 'cartao'>
+    <div style={{color : color} }>
+
+        <h1>O que é Festa?</h1>
+      <p>Festa é um evento social organizado com o propósito de celebrar, confraternizar ou marcar uma ocasião especial. Ela pode variar muito em tamanho, estilo e finalidade, indo desde pequenas reuniões familiares até grandes eventos públicos. As festas geralmente envolvem uma série de elementos como música, dança, comida, bebidas, decoração temática e atividades recreativas que promovem a interação e o entretenimento dos participantes.
+        As festas podem ter diferentes motivações, como comemorar aniversários, casamentos, feriados religiosos ou civis, conquistas pessoais e profissionais, rituais culturais, festivais tradicionais, entre outros. Elas funcionam como um momento de lazer, socialização e fortalecimento dos laços entre amigos, familiares e comunidades.
+        Além do aspecto festivo, as festas também desempenham um papel cultural importante, refletindo costumes, tradições e valores de uma sociedade. Elas são uma forma de expressão coletiva e ajudam na transmissão de identidade cultural entre gerações.</p>
+      </div>
+       <button onClick={() => {setColor(color == "rebeccapurple" ? "black": "rebeccapurple")}}>cor</button>  
+    </div>
+
+
 
       
     </>
