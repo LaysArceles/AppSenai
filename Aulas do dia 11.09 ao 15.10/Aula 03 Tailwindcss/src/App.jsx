cb9f58f7ce601c70ba6decce07c47888bb6d5881
LaysArceles <LaysArceles12@gmail.com>
2025-09-24 08:52:12 -0300

    Atualizando comandos

--- Aulas do dia 11.09 ao 15.10/Aula 03 Tailwindcss/src/App.jsx
+++ Aulas do dia 11.09 ao 15.10/Aula 03 Tailwindcss/src/App.jsx
@@ -7,27 +7,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <>
+      {/* Cor e tamanho da palavra */}
+      <h1 className='text-blue-500 text-6xl'>Testando tainwind</h1>
+      
+      {/* <h1 className="max-h-screen">Testando tainwind</h1>
+      <h1 className="min-h-screen">Testando tainwind</h1> */}
+
+      {/* Quadrado centrelizado na div */}
+      <div className="h-screen flex justify-center items-center">
+        <div className='w-52 h-52 bg-cyan-800'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
