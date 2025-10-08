f8b999b4ecb9816dc8c632bc2bf24fb123aac5a2
“LaysArceles” <LaysArceles12@gmail.com>
2025-10-08 11:41:33 -0300

    Finalizando o Projeto

--- Aulas do dia 11.09 ao 15.10/Aula 05 - Projeto/src/page/Persona.jsx
+++ Aulas do dia 11.09 ao 15.10/Aula 05 - Projeto/src/page/Persona.jsx
@@ -6,13 +6,22 @@ export const Persona = () =>
     const {slide } = local.state || {};
     if (!slide ) return <p>Nenhum Cart Selecionando.</p>
     return(
-        <div className="justify-center items-center-safe"> 
-            <h2>{slide.name}</h2>
+        <div className="h-screen">
+        <div className="justify-center text-5xl border-10 border-black ">
+            Characteristics of the characters
+        </div>
+         <div className="flex">
+        <div className="flex-col-reverse">
             <img src={slide.image} alt="" />
-            <p>house: {slide.house}</p>
+            </div>
+        <div className="justify-center  flex-col items-center-safe  border-50 border-black"> 
+            <h2>{slide.name}</h2>
+            <p>House: {slide.house}</p>
             <p>Date of birth: {slide.dateOfBirth}</p>
-            <p>ancestry: {slide.ancestry}</p>
-            <p>patronus: {slide.patronus}</p>
+            <p>Ancestry: {slide.ancestry}</p>
+            <p>Patronus: {slide.patronus}</p>
+        </div>
+        </div>   
         </div>
     )
 }
