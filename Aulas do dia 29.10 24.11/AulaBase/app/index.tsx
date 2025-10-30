f885f7019afd949572326c080225af2da7700eda
AmiltonMn <LaysArceles12@gmail.com>
2025-10-30 11:04:44 -0300

    Usando thouchableOpacity

--- Aulas do dia 29.10 24.11/AulaBase/app/index.tsx
+++ Aulas do dia 29.10 24.11/AulaBase/app/index.tsx
@@ -1,28 +1,76 @@
-import { Background } from "@react-navigation/elements";
-import { View,Text, StyleSheet } from "react-native";
+
+import { useState } from "react";
+import { View,Text, StyleSheet, TextInput, TouchableOpacity, Button,Image} from "react-native";
 
 export default function HomeScreen() {
+  const [text,setText] = useState("");
   return (
-      <View style = {{flex:1, justifyContent:"center", alignItems:'center'}}>
-      <View style ={styles.square}>
-        <Text style ={styles.colorBlue}> HEll </Text>
-      </View>
+  
+    <View style={{flex:1, justifyContent:"center", alignItems:'center'}}>
+        <View style={styles.square}>
+          <View style={styles.squaree}>
+            <View style={styles.squareee}>
+
+           <Text style={styles.colorBlue}> HELL</Text> 
+          <Image
+          style ={{width:150, height:150, borderRadius:155}} 
+          source = {require('../assets/images/f4.webp')}/>
+            </View>
+          </View>
+        </View>
+        <TextInput placeholder="Digite Seu caminho..." onChangeText={text => setText(text)}/>
+          <TouchableOpacity>
+            <View>
+            <Text>Botão</Text>
+            </View>
+          </TouchableOpacity>
+
+          <Button 
+          title = "Botão"
+          onPress = {()=> console.log('simple buton')} ></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   colorBlue:{
-    color: "blue",
+    color: "black",
     fontSize: 32,
-    textAlign: 'center'
+    textAlign: 'center',
+    position:'absolute',
+    zIndex: 1
   },
   square: 
   {
+    display:'flex',
     height:320,
     width: 320,
-    backgroundColor:"red", 
+    backgroundColor:"#8B0000", 
     borderRadius: 155,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  squaree: 
+  {
+    display:'flex',
+    height:260,
+    width: 260,
+    backgroundColor:"#800000", 
+    borderRadius: 150,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  squareee: 
+  {
+    zIndex: 0,
+    display:'flex',
+    height:160,
+    width: 160,
+    backgroundColor:"#7A0000", 
+    borderRadius: 150,
+    justifyContent: 'center',
+     alignItems: 'center'
+    
   }
 })
+
